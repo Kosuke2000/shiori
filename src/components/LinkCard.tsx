e768cd5d32630646d2c425b5edfac3c0d012f75a
Kosuke2000 <youuchiharu@gmail.com>
2022-01-23 18:22:05 +0900

    set eslint

---
+++ src/components/LinkCard.tsx
@@ -0,0 +1,9 @@
+import { VFC } from "react";
+
+interface LinkCardProps {}
+
+const LinkCard: VFC<LinkCardProps> = () => {
+  return <div>Link Card</div>;
+};
+
+export default LinkCard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
-import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 
+import LinkCard from "@/components/LinkCard";
+
+import type { NextPage } from "next";
+
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <LinkCard />;
 };
 
 export default Home;
