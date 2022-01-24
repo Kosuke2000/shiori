c255cd1dc7c417b8a2fb06ebd2f7ce87091e837c
Hajime Nakagawa <navyblue0914@icloud.com>
2022-01-24 10:28:31 +0900

    components/ui の追加、LinkCardをTevent配下に

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import Head from "next/head";
 
-import { LinkCard } from "@/components/LinkCard";
-import { Spacer } from "@/components/Spacer";
+import { LinkCard } from "@/components/model/TEvent/LinkCard";
+import { Spacer } from "@/components/model/ui/Spacer";
+
 import { mockOgpDataList } from "@/mocks/OgpData";
 
 import type { NextPage } from "next";
