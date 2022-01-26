be20ac7f1a8ab86dbb1dc51fa77d2ed535bcc812
Hajime Nakagawa <navyblue0914@icloud.com>
2022-01-26 19:36:23 +0900

    モックのDateを修正

--- src/mocks/Day.ts
+++ src/mocks/Day.ts
@@ -3,17 +3,17 @@ import { Day } from "@/types";
 import { mockTEventList0, mockTEventList1, mockTEventList2 } from "./TEvent";
 
 export const mockDay0: Day = {
-  date: new Date("2022-02-26 13:14:53"),
+  date: new Date("26 Febrary 2022 13:14 UTC"),
   events: mockTEventList0,
 };
 
 export const mockDay1: Day = {
-  date: new Date("2022-02-27 13:14:53"),
+  date: new Date("27 Febrary 2022 13:14 UTC"),
   events: mockTEventList1,
 };
 
 export const mockDay2: Day = {
-  date: new Date("2022-02-28 13:14:53"),
+  date: new Date("28 Febrary 2022 13:14 UTC"),
   events: mockTEventList2,
 };
 
--- src/mocks/TEvent.ts
+++ src/mocks/TEvent.ts
@@ -3,46 +3,46 @@ import { TEvent } from "@/types";
 // 初日の予定
 
 const mockTEvent0_0: TEvent = {
-  startAt: new Date("2022-01-26 13:14:53"),
+  startAt: new Date("26 January 2022 13:14 UTC"),
   src: "https://rtrp.jp/spots/0550bb70-868b-4157-af55-a4f45dd3f3de/",
   title: "シーサイドももち海浜公園",
 };
 const mockTEvent0_1: TEvent = {
-  startAt: new Date("2022-01-26 13:14:53"),
+  startAt: new Date("26 January 2022 13:14 UTC"),
   src: "https://tabelog.com/fukuoka/A4001/A400103/40055192/",
   title: "焼き鳥 あたらよ",
 };
 const mockTEvent0_2: TEvent = {
-  startAt: new Date("2022-01-26 20:20:55"),
+  startAt: new Date("26 January 2022 20:20 UTC"),
   src: "https://rtrp.jp/spots/36578ee3-cf37-4830-9b28-fad5962ec672/",
   title: "ナイトクルーズ",
 };
 const mockTEvent0_3: TEvent = {
-  startAt: new Date("2022-01-26 13:14:53"),
+  startAt: new Date("26 January 2022 13:14 UTC"),
   src: "https://www.hafh.com/properties/954",
   title: "ホテル",
 };
 
 // 2日目の予定
 const mockTEvent1_0: TEvent = {
-  startAt: new Date("2022-01-26 11:15:48"),
+  startAt: new Date("26 January 2022 11:15 UTC"),
   src: "https://www.mentaiju.com/",
   title: "めんたい重",
 };
 const mockTEvent1_1: TEvent = {
-  startAt: new Date("2022-01-26 16:15:48"),
+  startAt: new Date("26 January 2022 16:15 UTC"),
   src: "https://travel.biglobe.ne.jp/hotel/h019658/plan/",
   title: "温泉",
 };
 const mockTEvent1_2: TEvent = {
-  startAt: new Date("2022-01-26 19:15:55"),
+  startAt: new Date("26 January 2022 19:15 UTC"),
   src: "https://www.hafh.com/properties/989",
   title: "RE SORT",
 };
 
 // 3日目の予定
 const mockTEvent2_0: TEvent = {
-  startAt: new Date("2022-01-26 10:15:55"),
+  startAt: new Date("26 January 2022 10:15 UTC"),
   src: "https://www.fukuoka-airport.jp/",
   title: "空港",
 };
