2b9901dc62560299278c31fb05e458c6af8965e4
Kosuke2000 <youuchiharu@gmail.com>
2022-01-26 19:03:15 +0900

    モバイル版のローディングスケルトン実装

--- src/components/model/TEvent/Loading.tsx
+++ src/components/model/TEvent/Loading.tsx
@@ -1,10 +1,11 @@
 import { VFC } from "react";
 import Skeleton from "react-loading-skeleton";
+
 import "react-loading-skeleton/dist/skeleton.css";
 
-export const Loading: VFC = () => {
+export const LoadingDesktop: VFC = () => {
   return (
-    <div>
+    <div className="hidden md:block">
       <article className="flex justify-between h-40 rounded border border-gray-400 border-solid">
         <div className="flex flex-col justify-between p-5 w-3/5 hover:bg-gray-100">
           <h3 className="w-40 text-2xl truncate">
@@ -24,3 +25,33 @@ export const Loading: VFC = () => {
     </div>
   );
 };
+
+export const LoadingMobile = () => {
+  return (
+    <div className="md:hidden">
+      <article className="flex flex-col justify-between rounded border border-gray-400 border-solid">
+        <div className="object-cover w-full h-40 rounded">
+          <Skeleton className="object-cover w-full h-full" />
+        </div>
+
+        <div
+          className={`flex flex-col justify-between p-5 h-40  hover:bg-gray-100 w-full`}
+        >
+          <Skeleton className="text-xl truncate" />
+          <Skeleton className="overflow-hidden h-12 text-base text-gray-500" />
+          <div className="flex items-center">
+            <Skeleton className="text-base truncate" />
+          </div>
+        </div>
+      </article>
+    </div>
+  );
+};
+
+// Presentational Component Container
+export const Loading = () => (
+  <>
+    <LoadingDesktop />
+    <LoadingMobile />
+  </>
+);
